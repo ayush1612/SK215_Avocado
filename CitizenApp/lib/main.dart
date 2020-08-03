59f25b0c59ae364733eb51ff49fb3795f95b79c0
KritikaChoudhary <kritikac2410@gmail.com>
2020-08-03 15:20:17 +0530

    Modifications in main file

--- CitizenApp/lib/main.dart
+++ CitizenApp/lib/main.dart
@@ -1,18 +1,19 @@
-import 'package:citizen_app/Screens/HomePage.dart';
+//import 'package:citizen_app/Screens/HomePage.dart';
+import 'package:citizen_app/Screens/fingerprintSign_in.dart';
 import 'package:flutter/material.dart';
 import 'package:citizen_app/Screens/ProfilePage.dart';
 import 'package:citizen_app/Screens/HelpPage.dart';
 import 'package:citizen_app/Screens/FIRList.dart';
 import 'package:citizen_app/Screens/missingList.dart';
 import 'package:citizen_app/Screens/missing.dart';
-import 'package:citizen_app/Screens/pdfViewer.dart';
+//import 'package:citizen_app/Screens/pdfViewer.dart';
 import 'package:citizen_app/Screens/reportMissing.dart';
 
 void main() {
   runApp(new MaterialApp(
     theme: ThemeData.dark(),
     title: "Citizen App",
-    home: new HomePage(),
+    home: new FingerprintScan(),
     routes: {
       "firstatus": (context) => FIRStatus(),
       "missing": (context) => Missing(),
